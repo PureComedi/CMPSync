0554341f1ef21c17e31bb5ad2ad48d4caa28389f
PureComedi <64898881+PureComedi@users.noreply.github.com>
2023-03-07 19:06:55 +0100

    minor fixes and added bootscreen

--- discord.lua
+++ discord.lua
@@ -1,4 +1,6 @@
+local gpu = component.gpu
 local internet = require("internet")
+local component = require("component")
 local term = require("term")
 local json = require("json")
 local password = "passwordhere"
@@ -7,7 +9,53 @@ local headers = {
       ["Content-Type"] = "application/json",
       ["User-Agent"] = "MyBot (https://www.example.com, 1.0)"
 }
- 
+
+local screenWidth, screenHeight = gpu.getResolution()
+local centerX = math.floor(screenWidth / 2)
+local centerY = math.floor(screenHeight / 2)
+local duration = 2 -- Define the duration of the animation in seconds
+local frameRate = 60 -- Define the frame rate of the animation in frames per second
+local numFrames = duration * frameRate -- Define the number of frames in the animation
+local frameDelay = 1000 / frameRate -- Define the delay between frames in milliseconds
+local bootscrn = {
+"   _____   __  __   _____  ",
+"  / ____| |  \\/  | |  __ \\ ",
+" | |      | \\  / | | |__) |",
+" | |      | |\\/| | |  ___/ ",
+" | |____  | |  | | | |     ",
+"  \\_____| |_|  |_| |_|     ",
+"                            ",
+"            PCmdi™          "
+}
+
+gpu.fill(1, 1, screenWidth, screenHeight, " ")
+
+-- Define the alpha value of the art at each frame
+local alphas = {}
+for i = 1, numFrames do
+  local alpha = 1 - (i - 1) / (numFrames - 1)
+  alphas[i] = alpha
+end
+
+-- Loop over each frame of the animation
+for i = 1, numFrames do
+  -- Calculate the current alpha value of the art
+  local alpha = alphas[i]
+
+  -- Clear the screen
+  gpu.fill(1, 1, screenWidth, screenHeight, " ")
+
+  -- Print the boot screen logo with the current alpha value
+  for j, line in ipairs(ascii) do
+    local lineX = centerX - math.floor(#line / 2)
+    local lineY = centerY - math.ceil(#ascii / 2) + j - 1
+    gpu.setForeground(0xFFFFFF, alpha)
+    gpu.set(lineX, lineY, line)
+  end
+
+  os.sleep(frameDelay / 1000)
+end
+
 term.clear()
  
 print("Username:")
@@ -49,7 +97,7 @@ if attempt == password then
     }
   },
   username = "CMP",
-  avatar_url = "https://cdn.discordapp.com/attachments/795312360226947104/1078978654572396694/p.png?size=4096"
+  avatar_url = "https://cdn.discordapp.com/attachments/1082257996429668395/1082722647030378607/image.png?size=4096"
 }
  
   internet.request(url, json.encode(contents), headers, "POST")
@@ -74,7 +122,7 @@ if attempt == password then
       
       content = message,
       username = "CMP" .. " - " .. lusername,
-      avatar_url = "https://cdn.discordapp.com/attachments/795312360226947104/1078978654572396694/p.png?size=4096"
+      avatar_url = "https://cdn.discordapp.com/attachments/1082257996429668395/1082722647030378607/image.png?size=4096"
 }
  
     internet.request(url, json.encode(contents), headers, "POST")
@@ -91,7 +139,7 @@ if attempt == password then
     }
   },
   username = "CMP",
-  avatar_url = "https://cdn.discordapp.com/attachments/795312360226947104/1078978654572396694/p.png?size=4096"
+  avatar_url = "https://cdn.discordapp.com/attachments/1082257996429668395/1082722647030378607/image.png?size=4096"
 }
  
   internet.request(url, json.encode(contents), headers, "POST")
