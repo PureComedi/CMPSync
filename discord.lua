4d5ff45287353763001c8580d213699a98bcb01b
PureComedi <64898881+PureComedi@users.noreply.github.com>
2023-03-10 18:00:42 +0100

    Merged management
    Merged servers.lua and emojis.lua into discord.lua in the form of subcommands within the Settings command

--- discord.lua
+++ discord.lua
@@ -89,12 +89,183 @@ if attempt == password then
  
   while true do
  
+    ::MessageStart::
+
     local message = io.read()
- 
+
       if message == "Logout" then
         break
-    end
- 
+      
+      elseif message == "Settings" then
+        print("Select a setting to modify")
+        print("1. Servers")
+        print("2. Replacements")
+        local setting_choice = io.read()
+        if setting_choice == "1" then
+          local term = require "term"
+          local options = dofile("options.lua")
+           
+          -- helper function to save options to file
+          local function saveOptions()
+            local file = io.open("options.lua", "w")
+            file:write("return {\n")
+            for i, option in ipairs(options) do
+              file:write(string.format("  {name = %q, value = %q},\n", option.name, option.value))
+            end
+            file:write("}\n")
+            file:close()
+          end
+          
+          -- function to add a new option
+          local function addOption()
+            print("Enter a name for the new option:")
+            io.write()
+            local name = io.read()
+            print("Enter a value for the new option:")
+            io.write()
+            local value = io.read()
+            table.insert(options, {name = name, value = value})
+            saveOptions()
+            print("Option added.")
+          end
+          
+          -- function to remove an existing option
+          local function removeOption()
+            print("Enter the number of the option you want to remove:")
+            for i, option in ipairs(options) do
+              print(i .. ". " .. option.name)
+            end
+            io.write()
+            local choice = tonumber(io.read())
+            if choice and options[choice] then
+              table.remove(options, choice)
+              saveOptions()
+              print("Option removed.")    
+            else
+              print("Invalid choice.")
+            end
+          end
+          
+          -- function to list existing options
+          local function listOptions()
+            print("Existing options:")
+            for i, option in ipairs(options) do
+              print(i .. ". " .. option.name)
+            end
+          end
+          
+          -- main loop
+          while true do
+            print("\n")
+            print("Select an option:")
+            print("1. Add an option")
+            print("2. Remove an option")
+            print("3. List existing options")
+            print("4. Exit")
+            io.write()
+            local choice = tonumber(io.read())
+            if choice == 1 then
+              term.clear()
+              addOption()
+            elseif choice == 2 then
+              term.clear()
+              removeOption()
+            elseif choice == 3 then
+              term.clear()
+              listOptions()
+            elseif choice == 4 then
+              term.clear()
+              break
+            else
+              print("Invalid choice.")
+            end
+          end
+          goto MessageStart
+
+        elseif setting_choice == "2" then
+          local term = require "term"
+          local options = dofile("replacements.lua")
+          
+          -- helper function to save options to file
+          local function saveOptions()
+            local file = io.open("replacements.lua", "w")
+            file:write("return {\n")
+            for i, option in ipairs(options) do
+              file:write(string.format("  {%q, %q},\n", option[1], option[2]))
+            end
+            file:write("}\n")
+            file:close()
+          end
+          
+          -- function to add a new option
+          local function addOption()
+            print("Enter a name for the new option:")
+            io.write()
+            local name = io.read()
+            print("Enter a value for the new option:")
+            io.write()
+            local value = io.read()
+            table.insert(options, {name, value})
+            saveOptions()
+            print("Option added.")
+          end
+          
+          -- function to remove an existing option
+          local function removeOption()
+            print("Enter the number of the option you want to remove:")
+            for i, option in ipairs(options) do
+              print(i .. ". " .. option[1] .. " => " .. option[2])
+            end
+            io.write()
+            local choice = tonumber(io.read())
+            if choice and options[choice] then
+              table.remove(options, choice)
+              saveOptions()
+              print("Option removed.")
+            else
+              print("Invalid choice.")
+            end
+          end
+          
+          -- function to list existing options
+          local function listOptions()
+            print("Existing options:")
+            for i, option in ipairs(options) do
+              print(i .. ". " .. option[1])
+            end
+          end
+          
+          -- main loop
+          while true do
+            print("\n")
+            print("Select an option:")
+            print("1. Add an option")
+            print("2. Remove an option")
+            print("3. List existing options")
+            print("4. Exit")
+            io.write()
+            local choice = tonumber(io.read())
+            if choice == 1 then
+              term.clear()
+              addOption()
+            elseif choice == 2 then
+              term.clear()
+              removeOption()
+            elseif choice == 3 then
+              term.clear()
+              listOptions()
+            elseif choice == 4 then
+              term.clear()
+              break
+            else
+              print("Invalid choice.")
+            end
+          end
+          goto MessageStart
+        end
+        io.write()
+      end
+
     for i = 1, #replacements do
       if message == replacements[i][1] then
         message = replacements[i][2]
